0e00fa21f3ffabb8528f42e97b48e413f98567dc
SonicXTails <aedgy.master@mail.ru>
2024-02-13 17:51:59 +0300

    ---

---
+++ Massive2/Massive2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+int main() {
+
+    setlocale(LC_ALL, "RUS");
+
+    const int rows = 3;
+    const int cols = 5;
+
+    double arr[rows][cols];
+    double rowSum[rows] = { 0.0 };
+
+    // Ввод элементов массива
+    cout << "Введите элементы массива:" << endl;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << "arr[" << i << "][" << j << "]: ";
+            cin >> arr[i][j];
+            rowSum[i] += arr[i][j];  // Суммируем элементы строки
+        }
+    }
+
+    // Вычисление и вывод среднего арифметического элементов строк
+    cout << "Среднее арифметическое элементов строк:" << endl;
+    for (int i = 0; i < rows; i++) {
+        double average = rowSum[i] / cols;
+        cout << "Строка " << i << ": " << average << endl;
+    }
+
+    return 0;
+}
